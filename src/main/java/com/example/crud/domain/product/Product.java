aadc3f128db73cc0d1304885bda9dbd46fc70d6d
Renato Noronha <dev.renato10@gmail.com>
2024-06-13 11:45:46 -0300

    feat: create Product entity

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,26 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="crud")
+@Entity(name="crud")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Integer price_in_cents;
+    private Boolean active;
+
+    public Product(RequestProductDTO requestProductDTO){
+        this.name = requestProductDTO.name();
+        this.price_in_cents = requestProductDTO.price_in_cents();
+        this.active = true;
+    }
+}
